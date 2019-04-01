8c4f3961969544214dfcf0b258a012154810abc7
Alexander Aleksanyan <aleksanyanalek@gmail.com>
2019-03-31 17:06:28 -0700

    add more routes

--- src/main/java/luxury/cars/CarController.java
+++ src/main/java/luxury/cars/CarController.java
@@ -2,9 +2,8 @@ package luxury.cars;
 
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RestController()
 @RequestMapping("/")
@@ -20,6 +19,50 @@ public class CarController {
     @GetMapping("/cars")
     public ArrayList<Car> carsInTheLot() {
         return cars;
+    }
+
+    @GetMapping("/search")
+    public List<Car> searchForCars(@RequestParam(required = false) String make,
+                                   @RequestParam(required = false) String model,
+                                   @RequestParam(required = false) Integer year) {
+        return cars
+                .stream()
+                .filter(car -> {
+                   ArrayList<String> conditionals = new ArrayList<String>(3);
+
+                   if (year != null) {
+                       conditionals.add("year");
+                   }
+
+                   if (make != null) {
+                       conditionals.add("make");
+                   }
+
+                   if (model != null) {
+                       conditionals.add("model");
+                   }
+
+                   return conditionals
+                           .stream()
+                           .filter(conditional -> {
+                               if (conditional.equals("make")) {
+                                   return car.make.equals(make);
+                               }
+
+                               if (conditional.equals("model")) {
+                                   return car.model.equals(model);
+                               }
+
+                               if (conditional.equals("year")) {
+                                   return car.year.equals(year);
+                               }
+
+                               return true;
+                           })
+                           .collect(Collectors.toList())
+                           .size() == conditionals.size();
+               })
+               .collect(Collectors.toList());
     }
 
     @PostMapping("/cars")
